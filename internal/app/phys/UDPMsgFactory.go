c70610ded5cb499d1e43ac94c56da2c5470dfad6
ezmicken <smoggach@gmail.com>
2020-07-15 12:40:09 -0400

    [phys] add UDPMsgFactory for creating msgs and pooling them in the future

---
+++ internal/app/phys/UDPMsgFactory.go
@@ -0,0 +1,37 @@
+package phys
+
+import(
+  . "go-space-serv/internal/app/phys/msg/incoming"
+  . "go-space-serv/internal/app/phys/interface"
+  . "go-space-serv/internal/app/phys/types"
+)
+
+// TODO: pooling
+
+// Create msg, deserialize it, publish it, return size
+func CreateAndPublishMsg(packet []byte, head int, target chan UDPMsg, playerId string) int {
+  cmd := UDPCmd(packet[head])
+  if cmd == SYNC {
+    msg := &CmdMsg{}
+    head = msg.Deserialize(packet, head)
+    msg.SetPlayerId(playerId)
+    target <- msg
+  } else if cmd == ENTER {
+    msg := &CmdMsg{}
+    head = msg.Deserialize(packet, head)
+    msg.SetPlayerId(playerId)
+    target <- msg
+  } else if cmd == EXIT {
+    msg := &CmdMsg{}
+    head = msg.Deserialize(packet, head)
+    msg.SetPlayerId(playerId)
+    target <- msg
+  } else if cmd == MOVESHOOT {
+    msg := &MoveShootMsg{}
+    head = msg.Deserialize(packet, head)
+    msg.SetPlayerId(playerId)
+    target <- msg
+  }
+
+  return head
+}
