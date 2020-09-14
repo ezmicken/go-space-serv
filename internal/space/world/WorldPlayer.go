c7f94f8a0045c15e5d521cdd05e66dc857f9b1bf
ezmicken <smoggach@gmail.com>
2020-09-14 17:36:12 -0400

    [world] add comments to WorldPlayer.go

--- internal/space/world/WorldPlayer.go
+++ internal/space/world/WorldPlayer.go
@@ -1,7 +1,6 @@
 package world
 
 import (
-  "log"
   "github.com/akavel/polyclip-go"
   "github.com/google/uuid"
   "go-space-serv/internal/space/snet/tcp"
@@ -19,6 +18,7 @@ type WorldPlayer struct {
   view      polyclip.Polygon
 }
 
+// TODO: make sure the previous update finished first!
 func (p *WorldPlayer) Update(x, y uint16, worldMap *WorldMap) {
   p.X = x
   p.Y = y
@@ -26,6 +26,9 @@ func (p *WorldPlayer) Update(x, y uint16, worldMap *WorldMap) {
   doubleX := float64(x)
   doubleY := float64(y)
 
+  // Construct a square representing
+  // the player's view.
+  // TODO: use player view range stat
   view := polyclip.Polygon{{
     {doubleX - 32, doubleY - 32},
     {doubleX + 32, doubleY - 32},
@@ -36,13 +39,18 @@ func (p *WorldPlayer) Update(x, y uint16, worldMap *WorldMap) {
   // clip view to world map bounds
   view = worldMap.Poly.Construct(polyclip.INTERSECTION, view)
 
+  // Get Contour(s) that exist in view but not in
+  // the explored polygon.
   unexplored := view.Construct(polyclip.DIFFERENCE, p.explored)
 
+  // Clamp these contours to chunk size.
+  // Serialize the chunks and send to player.
   blocksMsgs := worldMap.SerializeChunks(unexplored)
   for _, m := range blocksMsgs {
     p.Tcp.Outgoing <- &m
   }
 
+  // Add unexplored to explored.
   p.explored = p.explored.Construct(polyclip.UNION, view)
   return
 }
