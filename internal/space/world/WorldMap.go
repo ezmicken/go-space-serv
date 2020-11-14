e85061f73ca6d0443f4786ad3828ed03430e85f1
ezmicken <smoggach@gmail.com>
2020-11-14 11:49:21 -0500

    WorldMap implements GetBlockRects copied from client.

--- internal/space/world/WorldMap.go
+++ internal/space/world/WorldMap.go
@@ -8,8 +8,10 @@ import (
   "errors"
 
   "github.com/akavel/polyclip-go"
+  "github.com/go-gl/mathgl/mgl32"
 
   "go-space-serv/internal/space/world/msg"
+  "go-space-serv/internal/space/geom"
 )
 
 // TODO: move to worldinfo
@@ -17,12 +19,23 @@ const RESOLUTION float32 = 32
 const SPAWNX uint32 = 64
 const SPAWNY uint32 = 64
 
+const NW int = 0
+const NE int = 1
+const SE int = 2
+const SW int = 3
+
 type WorldMap struct {
   info WorldInfo
   chunker *Chunker
   sizeInBlocks float64
 
   Poly polyclip.Polygon
+}
+
+type chip struct {
+  blocks []byte
+  box geom.RectInt
+  chunkId uint16
 }
 
 func NewWorldMap(name string) (*WorldMap, error) {
@@ -85,11 +98,194 @@ func (wm *WorldMap) serializeChunk(x, y int, id uint16) msg.BlocksMsg {
 
   fileId := uint16(math.Floor(float64(uint32(id) / wm.info.ChunksPerFile)))
 
-  serializedChunk := wm.chunker.GetChunk(id, fileId)
+  serializedChunk := wm.chunker.GetZippedChunk(id, fileId)
   blocksMsg.Data = append([]byte{}, serializedChunk...)
 
   return blocksMsg
 }
+
+func (wm *WorldMap) GetBlockRects(playerBox geom.Rect) []geom.Rect {
+  // Get playerBox in block coordinates
+  blockMinX, blockMinY := wm.GetCellFromPosition(playerBox.X, playerBox.Y)
+  blockMaxX, blockMaxY := wm.GetCellFromPosition(playerBox.X + playerBox.W, playerBox.Y + playerBox.H)
+
+  blockMinX -= 1
+  blockMinY -= 1
+  blockW := blockMaxX - blockMinX + 2
+  blockH := blockMaxY - blockMinY + 2
+  blockBox := geom.NewRectInt(blockMinX, blockMinY, blockW, blockH)
+
+  playerBox.X = float32(blockMinX) * RESOLUTION
+  playerBox.Y = float32(blockMinY) * RESOLUTION
+  playerBox.W = float32(blockW) * RESOLUTION
+  playerBox.H = float32(blockH) * RESOLUTION
+  playerMaxX := playerBox.X + playerBox.W
+  playerMaxY := playerBox.Y + playerBox.H
+
+  actualWorldSize := float32(wm.info.Size * wm.info.ChunkSize) * RESOLUTION
+
+  var invalidChip chip
+  invalidChip.blocks = nil
+  invalidChip.box = geom.NewRectInt(0, 0, 0, 0)
+
+  var chipNW = invalidChip
+  var chipNE = invalidChip
+  var chipSE = invalidChip
+  var chipSW = invalidChip
+
+  if playerBox.X >= 0 && playerMaxY <= actualWorldSize {
+    chipNW = wm.getChip(blockBox, mgl32.Vec3{playerBox.X, playerMaxY, 0}, NW)
+  }
+
+  if playerMaxX <= actualWorldSize && playerMaxY <= actualWorldSize {
+    chipNE = wm.getChip(blockBox, mgl32.Vec3{playerMaxX, playerMaxY, 0}, NE)
+  }
+
+  if playerMaxX <= actualWorldSize && playerBox.X >= 0 {
+    chipSE = wm.getChip(blockBox, mgl32.Vec3{playerMaxX, playerBox.Y, 0}, SE)
+  }
+
+  if playerBox.X >= 0 && playerBox.Y >= 0 {
+    chipSW = wm.getChip(blockBox, mgl32.Vec3{playerBox.X, playerBox.Y, 0}, SW)
+  }
+
+  blocks := make([]byte, blockBox.W * blockBox.H)
+  numSolid := len(blocks)
+
+  // Best case -- all chips cover same chunk.
+  if chipNW.chunkId == chipNE.chunkId && chipNW.chunkId == chipSE.chunkId && chipNW.chunkId == chipSW.chunkId {
+    if chipNW.blocks == nil {
+      return nil
+    }
+
+    i := 0
+    for y := 0; y < chipNW.box.H; y++ {
+      for x := 0; x < chipNW.box.W; x++ {
+        color := chipNW.blocks[(int(wm.info.ChunkSize) * (y + chipNW.box.Y)) + (x + chipNW.box.X)]
+        if color <= 0 {
+          numSolid--
+        }
+        blocks[i] = color
+        i++
+      }
+    }
+  } else {
+    if chipNW.blocks != nil {
+      numSolid -= wm.transformBlocks(blockBox, chipNW, blocks, NW)
+    }
+    if chipNE.blocks != nil && chipNE.chunkId != chipNW.chunkId {
+      numSolid -= wm.transformBlocks(blockBox, chipNE, blocks, NE)
+    }
+    if chipSE.blocks != nil && chipSE.chunkId != chipNW.chunkId && chipSE.chunkId != chipNE.chunkId {
+      numSolid -= wm.transformBlocks(blockBox, chipSE, blocks, SE)
+    }
+    if chipSW.blocks != nil && chipSW.chunkId != chipNW.chunkId && chipSW.chunkId != chipNE.chunkId && chipSW.chunkId != chipSE.chunkId {
+      numSolid -= wm.transformBlocks(blockBox, chipSW, blocks, SW)
+    }
+  }
+
+  // Create a rect in world space for each solid block
+  if numSolid > 0 {
+    result := make([]geom.Rect, numSolid)
+    iRes := 0
+    iBlx := 0
+    for y := 0; y < blockBox.H; y++ {
+      for x := 0; x < blockBox.W; x++ {
+        if blocks[iBlx] > 0 {
+          result[iRes] = geom.NewRect(float32(blockBox.X + x) * RESOLUTION, float32(blockBox.Y + y) * RESOLUTION, RESOLUTION, RESOLUTION)
+          iRes++
+        }
+        iBlx++
+      }
+    }
+
+    return result
+  }
+
+  return nil
+}
+
+func (wm *WorldMap) transformBlocks(blockBox geom.RectInt, ch chip, blocks []byte, quad int) int {
+  numSolid := 0
+
+  for y := 0; y < ch.box.H; y++ {
+    for x := 0; x < ch.box.W; x++ {
+      color := ch.blocks[(int(wm.info.ChunkSize) * (y + ch.box.Y)) + (x + ch.box.X)]
+      if color <= 0 {
+        numSolid--
+      }
+
+      oX := blockBox.W - ch.box.W
+      oY := blockBox.H - ch.box.H
+      idx := -1
+
+      if quad == NW {
+        idx = (blockBox.W * oY) + (blockBox.W * y) + x
+      } else if quad == NE {
+        idx = (blockBox.W * oY) + (blockBox.W * y) + oX + x
+      } else if quad == SE {
+        idx = (blockBox.W * y) + oX + x
+      } else if quad == SW {
+        idx = (blockBox.W * y) + x
+      } else {
+        panic("transformBlocks encountered invalid Quadrant")
+      }
+
+      blocks[idx] = color
+    }
+  }
+
+  return numSolid
+}
+
+func (wm *WorldMap) getChip(blockBox geom.RectInt, worldPos mgl32.Vec3, quad int) chip {
+  chunkCellX := int(math.Floor(float64(worldPos.X() / (float32(wm.info.ChunkSize) * RESOLUTION))))
+  chunkCellY := int(math.Floor(float64(worldPos.Y() / (float32(wm.info.ChunkSize) * RESOLUTION))))
+  var c chip
+  c.chunkId = uint16((chunkCellY * int(wm.info.Size)) + chunkCellX)
+  fileId := uint16(math.Floor(float64(uint32(c.chunkId) / wm.info.ChunksPerFile)))
+  c.blocks = wm.chunker.GetChunk(c.chunkId, fileId)
+
+  var texX int
+  var texY int
+  var texW int
+  var texH int
+  chunkSize := int(wm.info.ChunkSize)
+
+  if quad == NW || quad == SW {
+    texX = blockBox.X - (chunkCellX * chunkSize)
+    texW = blockBox.W
+    if texX + blockBox.W > chunkSize {
+      texW = chunkSize - texX
+    }
+  } else {
+    texX = (blockBox.X + blockBox.W) - (chunkCellX * chunkSize) - blockBox.W
+    texW = blockBox.W
+    if texX < 0 {
+      texW += texX
+      texX = 0
+    }
+  }
+
+  if quad == SW || quad == SE {
+    texY = blockBox.Y - (chunkCellY * chunkSize)
+    texH = blockBox.H
+    if texY + blockBox.H > chunkSize {
+      texH = chunkSize - texY
+    }
+  } else {
+    texY = (blockBox.Y + blockBox.H) - (chunkCellY * chunkSize) - blockBox.H
+    texH = blockBox.H
+    if texY < 0 {
+      texH += texY
+      texY = 0
+    }
+  }
+  c.box = geom.NewRectInt(texX, texY, texW, texH)
+
+  return c
+}
+
 
 // Assumes bb is clamped to chunks
 func (wm *WorldMap) Explore(bb polyclip.Rectangle) []msg.BlocksMsg {
