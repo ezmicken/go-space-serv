981cb825215cf2aa1bb8525efdfb35974bacc643
ezmicken <smoggach@gmail.com>
2020-11-21 18:45:27 -0500

    Add RectOverlap function to Rect for determining overlapping area of rectangles.

--- internal/space/geom/Rect.go
+++ internal/space/geom/Rect.go
@@ -1,5 +1,9 @@
 package geom
 
+import(
+  "github.com/chewxy/math32"
+)
+
 type Rect struct {
   X float32
   Y float32
@@ -14,4 +18,18 @@ func NewRect(x, y, w, h float32) Rect {
   r.W = w
   r.H = h
   return r
+}
+
+func RectOverlap(a, b Rect) float32 {
+  axMax := a.X + a.W
+  ayMax := a.Y + a.H
+  bxMax := b.X + b.W
+  byMax := b.Y + b.H
+
+  overlapX := math32.Max(a.X, b.X)
+  overlapY := math32.Max(a.Y, b.Y)
+  overlapW := (math32.Min(axMax, bxMax) - overlapX)
+  overlapH := (math32.Min(ayMax, byMax) - overlapY)
+
+  return overlapW * overlapH
 }
